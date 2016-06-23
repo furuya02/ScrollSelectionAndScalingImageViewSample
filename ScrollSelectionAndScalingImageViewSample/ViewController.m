5727f0a8d9bfe0e5468904e14fe097fe2ff3e364
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2016-06-23 16:51:32 +0900

    コメントアウトされているコードを削除

--- ScrollSelectionAndScalingImageViewSample/ViewController.m
+++ ScrollSelectionAndScalingImageViewSample/ViewController.m
@@ -14,8 +14,6 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *selectView;
 @property (weak, nonatomic) IBOutlet UIScrollView *selectScrollView;
-//@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-//@property (weak, nonatomic) IBOutlet UIScrollView *imageScroolView;
 @property (weak, nonatomic) IBOutlet ImageScrollView *imageScrollView;
 @end
 
@@ -23,26 +21,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    //_imageScroolView.delegate = self;
-
-    //_imageScroolView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    //_imageScroolView.pagingEnabled = NO;
-    //_imageScroolView.bouncesZoom = NO;
-    //_imageScroolView.showsHorizontalScrollIndicator = NO;
-    //_imageScroolView.showsVerticalScrollIndicator = NO;
-//
-//
-//    _imageScroolView.minimumZoomScale = 4.0f;
-
-    //_imageScroolView.minimumZoomScale = 1.0;
-    //_imageScroolView.maximumZoomScale = 4.0;
-
 }
 
 - (void)viewDidAppear:(BOOL)animated{
-
-    //UIScrollView.contentSizeは、表示する写真の数に応じて変化させる
 
     // 写真を配置する　元写真の大きさからサイズを決定する
     const int baseSize = 100;
@@ -53,9 +34,7 @@
         NSString *photoName = [NSString stringWithFormat:@"%3.3d.jpg",i+1];
         UIImage *image = [UIImage imageNamed:photoName];
 
-
         UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-
 
         CGSize size = image.size;
         if ( size.width < size.height){ // portrait
@@ -82,42 +61,8 @@
     if (touch.view.tag > 0) {
         NSString *photoName = [NSString stringWithFormat:@"%3.3ld.jpg",touch.view.tag];
         [_imageScrollView setImage:[UIImage imageNamed:photoName]];
-
-//        CGSize size = _imageView.image.size;
-//        // imageViewのサイズがscrollView内に収まるように調整
-//        float wrate = _imageScroolView.frame.size.width / size.width;
-//        float hrate = _imageScroolView.frame.size.height / size.height;
-//        float rate = MIN(wrate,hrate);
-//
-//
-//        _imageView.frame = CGRectMake(0, 0, size.width * rate, size.height * rate);
-//        _imageScroolView.contentSize = _imageView.frame.size;
-//        [self updateScrollInset];
     }
 }
-
-//- (UIView*)viewForZoomingInScrollView:(UIScrollView*)scrollView
-//{
-//    return _imageView;
-//}
-//
-//- (void)scrollViewDidZoom:(UIScrollView*)scrollView
-//{
-//    [self updateScrollInset];
-//}
-
-//- (void) updateScrollInset {
-//    // imageViewの大きさからcontentInsetを再計算
-//    // なお、0を下回らないようにする
-////    _imageScroolView.contentInset = UIEdgeInsetsMake(
-////                                               max((scrollView.frame.height - imageView.frame.height)/2, 0),
-////                                               max((scrollView.frame.width - imageView.frame.width)/2, 0),
-////                                               0,
-////                                               0
-////                                               );
-//}
-
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
